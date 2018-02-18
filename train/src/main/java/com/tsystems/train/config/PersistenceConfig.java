6b68887a2bc50baab115e72ee4343147d59a8d99
pcsaic <pcsaic@yandex.ru>
2018-02-18 23:50:55 +0300

    Station commit

--- train/src/main/java/com/tsystems/train/config/PersistenceConfig.java
+++ train/src/main/java/com/tsystems/train/config/PersistenceConfig.java
@@ -13,7 +13,6 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
-
 import javax.sql.DataSource;
 
 @Configuration
--- train/src/main/java/com/tsystems/train/repository/StationRepository.java
+++ train/src/main/java/com/tsystems/train/repository/StationRepository.java
@@ -1,7 +1,14 @@
 package com.tsystems.train.repository;
 
+import com.tsystems.train.entity.Station;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
+
 
 @Repository
-public interface StationRepository {
+public interface StationRepository extends JpaRepository <Station, String> {
+
+    Station findByName(String name);
+    List<Station> findByNameIsLike(String name);
 }
